c5bdbea5999507fe616d34884c5fde2590a881e1
zzx <2543673022@qq.com>
2026-07-24 23:25:57 +0800

    表单约束-新增交易

--- dlyk-front/src/view/TranRecordView.vue
+++ dlyk-front/src/view/TranRecordView.vue
@@ -1,8 +1,14 @@
 <template>
   <el-button type="success" @click="goBack">返回</el-button>
 
-  <el-form ref="tranRefForm" :model="tranQuery" label-width="120px" style="margin-top: 20px; width: 600px;">
-    <el-form-item label="客户">
+  <el-form
+      ref="tranRefForm"
+      :model="tranQuery"
+      :rules="tranRules"
+      label-width="120px"
+      style="margin-top: 20px; width: 600px;">
+
+    <el-form-item label="客户" prop="customerId">
       <el-select v-model="tranQuery.customerId" style="width: 100%;">
         <el-option v-for="item in customerList"
                    :key="item.id"
@@ -10,14 +16,18 @@
                    :value="item.id"/>
       </el-select>
     </el-form-item>
-    <el-form-item label="交易金额">
+
+    <el-form-item label="交易金额" prop="money">
       <el-input v-model="tranQuery.money"/>
     </el-form-item>
-    <el-form-item label="预计成交日期">
-      <el-date-picker v-model="tranQuery.expectedDate" type="datetime" placeholder="选择时间" style="width: 100%;"
+
+    <el-form-item label="预计成交日期" prop="expectedDate">
+      <el-date-picker v-model="tranQuery.expectedDate" type="datetime"
+        placeholder="选择时间" style="width: 100%;"
         value-format="YYYY-MM-DD HH:mm:ss"/>
     </el-form-item>
-    <el-form-item label="交易阶段">
+
+    <el-form-item label="交易阶段" prop="stage">
       <el-select v-model="tranQuery.stage" style="width: 100%;">
         <el-option v-for="item in stageList"
                    :key="item.id"
@@ -25,13 +35,17 @@
                    :value="item.id"/>
       </el-select>
     </el-form-item>
-    <el-form-item label="描述">
+
+    <el-form-item label="描述" prop="description">
       <el-input v-model="tranQuery.description" type="textarea"/>
     </el-form-item>
-    <el-form-item label="下次联系时间">
-      <el-date-picker v-model="tranQuery.nextContactTime" type="datetime" placeholder="选择时间" style="width: 100%;"
+
+    <el-form-item label="下次联系时间" prop="nextContactTime">
+      <el-date-picker v-model="tranQuery.nextContactTime" type="datetime"
+        placeholder="选择时间" style="width: 100%;"
         value-format="YYYY-MM-DD HH:mm:ss"/>
     </el-form-item>
+
     <el-form-item>
       <el-button type="primary" @click="submit">提交</el-button>
       <el-button @click="goBack">取消</el-button>
@@ -51,7 +65,29 @@ export default defineComponent({
     return {
       tranQuery: {},
       customerList: [],
-      stageList: []
+      stageList: [],
+      tranRules: {
+        customerId: [
+          { required: true, message: '请选择客户', trigger: 'change' }
+        ],
+        money: [
+          { required: true, message: '请输入交易金额', trigger: 'blur' },
+          { pattern: /^[0-9]+(\.[0-9]{1,2})?$/, message: '金额格式不正确', trigger: 'blur' }
+        ],
+        expectedDate: [
+          { required: true, message: '请选择预计成交日期', trigger: 'change' }
+        ],
+        stage: [
+          { required: true, message: '请选择交易阶段', trigger: 'change' }
+        ],
+        description: [
+          { required: true, message: '请输入交易描述', trigger: 'blur' },
+          { min: 2, max: 255, message: '描述长度为2-255个字符', trigger: 'blur' }
+        ],
+        nextContactTime: [
+          { required: true, message: '请选择下次联系时间', trigger: 'change' }
+        ]
+      }
     }
   },
 
@@ -97,21 +133,24 @@ export default defineComponent({
     },
 
     submit() {
-      let isEdit = this.tranQuery.id > 0;
-      // 用 FormData，后端没有 @RequestBody，期望的就是表单数据
-      let formData = new FormData();
-      for (let field in this.tranQuery) {
-        if (this.tranQuery[field] !== null && this.tranQuery[field] !== undefined && this.tranQuery[field] !== '') {
-          formData.append(field, this.tranQuery[field]);
-        }
-      }
-      let req = isEdit ? doPut("/api/tran", formData) : doPost("/api/tran", formData);
-      req.then(resp => {
-        if (resp.data.code === 200) {
-          messageTip(isEdit ? "编辑成功" : "新增成功", "success");
-          this.$router.push("/dashboard/tran");
-        } else {
-          messageTip(isEdit ? "编辑失败" : "新增失败", "error");
+      this.$refs.tranRefForm.validate((isValid) => {
+        if (isValid) {
+          let isEdit = this.tranQuery.id > 0;
+          let formData = new FormData();
+          for (let field in this.tranQuery) {
+            if (this.tranQuery[field] !== null && this.tranQuery[field] !== undefined && this.tranQuery[field] !== '') {
+              formData.append(field, this.tranQuery[field]);
+            }
+          }
+          let req = isEdit ? doPut("/api/tran", formData) : doPost("/api/tran", formData);
+          req.then(resp => {
+            if (resp.data.code === 200) {
+              messageTip(isEdit ? "编辑成功" : "新增成功", "success");
+              this.$router.push("/dashboard/tran");
+            } else {
+              messageTip(isEdit ? "编辑失败" : "新增失败", "error");
+            }
+          })
         }
       })
     },
